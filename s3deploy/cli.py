452f7d33043dd28488ac284591ffcc37ba3144fc
Marc Monnerat <procrastinatio@gmail.com>
2019-02-03 09:57:07 +0100

    simplifiying branch listing

--- s3deploy/cli.py
+++ s3deploy/cli.py
@@ -7,67 +7,65 @@ import yaml
 from s3 import init_connection, list_version, version_info
 
 
-
 def CommandWithConfigFile(config_file_param_name):
-
     class CustomCommandClass(click.Command):
-
         def invoke(self, ctx):
             config_file = ctx.params[config_file_param_name]
             if config_file is not None:
-              try:
-                with open(config_file) as f:
-                    config_data = yaml.load(f)
-                    print(config_data)
-                    for param, value in ctx.params.items():
-                        if value is None and param in config_data:
-                            ctx.params[param] = config_data[param]
-              except IOError:
-                  print("Cannot open file {}".format(config_file))
-                  sys.exit()
+                try:
+                    with open(config_file) as f:
+                        config_data = yaml.load(f)
+                        print(config_data)
+                        for param, value in ctx.params.items():
+                            if value is None and param in config_data:
+                                ctx.params[param] = config_data[param]
+                except IOError:
+                    print("Cannot open file {}".format(config_file))
+                    sys.exit()
 
             return super(CustomCommandClass, self).invoke(ctx)
 
     return CustomCommandClass
 
+
 @click.group()
 def cli():
     pass
+
 
 @click.group()
 @click.option("--config_file", type=click.Path())
 @click.pass_context
 def another(ctx, config_file):
-    ctx.obj = {
-        'config_file': config_file,
-    }
-    
-    
-@cli.command(name='info', cls=CommandWithConfigFile('config_file'))
+    ctx.obj = {"config_file": config_file}
+
+
+@cli.command(name="info", cls=CommandWithConfigFile("config_file"))
 @click.option("--bucket", "-b", type=str, required=True)
-@click.option("--config_file", "-c", type=click.Path(),  required=True)
-@click.argument('s3_path')
-def info_version(s3_path,bucket, config_file):
+@click.option("--config_file", "-c", type=click.Path(), required=True)
+@click.argument("s3_path")
+def info_version(s3_path, bucket, config_file):
     print("bucket: {}".format(bucket))
     print("config_file: {}".format(config_file))
-    
+
     s3, s3client, bucket = init_connection(bucket)
-    
+
     version_info(s3, bucket, s3_path)
 
-@cli.command(name='list', cls=CommandWithConfigFile('config_file'))
+
+@cli.command(name="list", cls=CommandWithConfigFile("config_file"))
 @click.option("--bucket", "-b", type=str, required=True)
-@click.option("--config_file", "-c", type=click.Path(),  required=True)
+@click.option("--config_file", "-c", type=click.Path(), required=True)
 @click.pass_context
 def list_versions(ctx, bucket, config_file):
     print("bucket: {}".format(bucket))
     print("config_file: {}".format(ctx.obj))
-    
+
     s3, s3client, bucket = init_connection(bucket)
-    
+
     print(list_version(bucket))
 
 
-if __name__ == '__main__':
-        cli()
-        #main('my_arg --config_file dummy.yaml'.split()) 
+if __name__ == "__main__":
+    cli()
+    # main('my_arg --config_file dummy.yaml'.split())
--- s3deploy/s3.py
+++ s3deploy/s3.py
@@ -11,50 +11,55 @@ def init_connection(bucket_name):
     try:
         session = boto3.session.Session()
     except botocore.exceptions.BotoCoreError as e:
-        print('Cannot establish connection to bucket "%s". Check you credentials.' % bucket_name)
+        print(
+            'Cannot establish connection to bucket "%s". Check you credentials.'
+            % bucket_name
+        )
         print(e)
         sys.exit(1)
 
-    s3client = session.client('s3', config=boto3.session.Config(signature_version='s3v4'))
-    s3 = session.resource('s3', config=boto3.session.Config(signature_version='s3v4'))
+    s3client = session.client(
+        "s3", config=boto3.session.Config(signature_version="s3v4")
+    )
+    s3 = session.resource("s3", config=boto3.session.Config(signature_version="s3v4"))
 
     bucket = s3.Bucket(bucket_name)
     return (s3, s3client, bucket)
 
+
 # s3://$S3_MF_GEOADMIN3_INT/mom_webmerc_search/1812041829/
 
 # aws s3 ls s3://$S3_MF_GEOADMIN3_PROD/master/e9c53df/1807181420/
-''' Prefix master/
+""" Prefix master/
     CommonPrefixes: master/e6eacb9/
     Prefixes  None
-'''
+"""
+
 
 def clean_path(path, previous_path):
-    return path.replace(previous_path, '').replace('/', '')
+    if previous_path is not None:
+        cleaned_path = path.replace(previous_path, "")
+    return cleaned_path.replace("/", "")
 
-def get_subitems(bucket, prefix=''):
+
+def get_subitems(bucket, prefix=""):
     items = []
-    builds = bucket.meta.client.list_objects(Bucket=bucket.name,
-                                   Prefix=prefix,
-                                   Delimiter='/')
+    builds = bucket.meta.client.list_objects(
+        Bucket=bucket.name, Prefix=prefix, Delimiter="/"
+    )
 
-    for v in builds.get('CommonPrefixes'):
-        build = v.get('Prefix')
-       
-        if prefix is not None:
-            build = clean_path(build, prefix)
-        else:
-            build = v.replace('/','')
+    for v in builds.get("CommonPrefixes"):
+        build = v.get("Prefix")
+        build = clean_path(build, prefix)
         items.append(build)
-        
+
     return items
 
 
-
-def get_version_info(s3,bucket, s3_path):
-    print('App version is: %s' % s3_path)
-    version_target = s3_path.split('/')[2]
-    obj = s3.Object(bucket.name, '%s/%s/info.json' % (s3_path, version_target))
+def get_version_info(s3, bucket, s3_path):
+    print("App version is: %s" % s3_path)
+    version_target = s3_path.split("/")[2]
+    obj = s3.Object(bucket.name, "%s/%s/info.json" % (s3_path, version_target))
     try:
         content = obj.get()["Body"].read()
         raw = _unzip_data(content)
@@ -66,44 +71,83 @@ def get_version_info(s3,bucket, s3_path):
     return data
 
 
-def version_info(s3,bucket, s3_path):
+def version_info(s3, bucket, s3_path):
     info = get_version_info(s3, bucket, s3_path)
     if info is None:
-        print('No info for version %s' % s3_path)
+        print("No info for version %s" % s3_path)
         sys.exit(1)
     for k in info.keys():
-        print('%s: %s' % (k, info[k]))
+        print("%s: %s" % (k, info[k]))
 
+'''
+1/ Not a official path for branch fix_4200
+
+Named branch: fix_4200 (version: 1803190954)
+Named branch: fix_4200 (version: 1803191110)
+Named branch: fix_4200 (version: 1803201226)
+Named branch: fix_4200 (version: 1803201352)
+Not a official path for branch fix_4200
+
+$ aws s3 ls s3://$S3_MF_GEOADMIN3_INT/fix_4200/
+                           PRE 1803190954/
+                           PRE 1803191110/
+                           PRE 1803201226/
+                           PRE 1803201352/
+                           PRE src/
+2018-03-20 14:55:30      71474 404.html
+2018-03-20 14:55:30         23 checker
+2018-03-20 14:55:30       4524 embed.html
+2018-03-20 14:55:30        326 favicon.ico
+2018-03-19 10:57:44        372 geoadmin.1803190954.appcache
+
+
+
+
+
+2/ Full version: gal_cloudfront/110ba87/1472481476
+
+shas 110ba87, 1e37ff9/ et e70190b/
+many version
+
+
+
+
+'''
 def version_exists(s3_path):
     files = bucket.objects.filter(Prefix=str(s3_path)).all()
     return len(list(files)) > 0
 
-def list_version(bucket):
-   
-        
-    for branch in get_subitems(bucket, prefix=''):
 
-        
-        if re.search(r'^\D', branch):
-        
-            shas = get_subitems(bucket, prefix=branch + '/')
+def list_version(bucket):
+
+    for branch in get_subitems(bucket, prefix=""):
+
+        #if re.search(r"^\D", branch):  # branch's name may have number!!!
+        if re.search(r"^[a-z0-9_-]", branch):
+
+            shas = get_subitems(bucket, prefix=branch + "/")
 
             for sha in shas:
 
-                    nice_sha = clean_path(sha, branch)
-                    # Full version path to display
+                nice_sha = clean_path(sha, branch)
+                # Full version path to display
 
-                    if re.match('[0-9a-f]{7}$', nice_sha) is not None:
-                       
-                        builds = get_subitems(bucket, prefix=branch + '/' + nice_sha + '/')
-                        for build in builds:
-                        
-                            print('Full version: %s/%s/%s' % (branch,
-                                                             nice_sha,
-                                                             clean_path(build, nice_sha) ))
-                    else:
-                        # Matching a version of the deployed branch
-                        if re.match('[0-9]{10}', nice_sha):
-                            print('Named branch: %s (version: %s)' % (branch.replace('/', ''), nice_sha))
+                if re.match("[0-9a-f]{7}$", nice_sha) is not None:
+
+                    builds = get_subitems(bucket, prefix=branch + "/" + nice_sha + "/")
+                    for build in builds:
+                        if re.match("[0-9]{10}", build):
+
+                          print(
+                            "Full version: %s/%s/%s"
+                            % (branch, nice_sha, clean_path(build, nice_sha))
+                          )
+                else:
+                    # Matching a version of the deployed branch (it is olds ?)
+                    if re.match("[0-9]{10}", nice_sha):
+                        print(
+                            "Named branch: %s (version: %s)"
+                            % (branch.replace("/", ""), nice_sha)
+                        )
             else:
-                print('Not a official path for branch %s' % branch)
+                print("Not a official path for branch %s" % branch)
