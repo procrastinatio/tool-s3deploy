10d4ae358b358f4758ef42af6551426977be6def
Marc Monnerat <procrastinatio@gmail.com>
2019-02-05 13:05:10 +0100

    add bucket name

--- s3deploy/cli.py
+++ s3deploy/cli.py
@@ -12,19 +12,17 @@ import json
 
 from repository import  SimpleRepo, GeoAdminRepo, RepoManager
 
-
-            
 pass_repo = click.make_pass_decorator(RepoManager)
 
 @click.group()
-@click.option('--repo-home', envvar='REPO_HOME', default='.repo')
+@click.option('--bucket-name','-b', 'bucket_name',  envvar='BUCKET_NAME', default='.repo')
 @click.option('--debug/--no-debug', default=False,
               envvar='REPO_DEBUG')
 @click.option("--config_file", "-c", type=click.Path(), required=True)
 @click.pass_context
-def cli(ctx, config_file, repo_home, debug):
+def cli(ctx, config_file, bucket_name, debug):
     
-    ctx.obj = SimpleRepo(repo_home, debug)
+    ctx.obj = SimpleRepo(bucket_name, debug)
     
     with open(config_file) as f:
         config_data = yaml.load(f)
@@ -35,17 +33,17 @@ def cli(ctx, config_file, repo_home, debug):
     
     
     
-    ctx.obj= RepoManager('GeoAdminRepo', repo_home, debug)
+    ctx.obj= RepoManager('GeoAdminRepo', bucket_name, debug)
     #print('Repo', r)
 
 
     
-@cli.command()
+@cli.command(name='list')
 @click.argument('src')
 @click.argument('dest', required=False)
 #@click.pass_obj
 @pass_repo
-def clone(repo, src, dest):
+def list_versions(repo, src, dest):
     print(repo, src, dest, repo.archive_engine.generate())
 
 
